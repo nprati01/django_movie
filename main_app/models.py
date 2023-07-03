d4ffa80acc1c03191c6416594f3af191927b7d87
nprati01 <nicole.e.prati@gmail.com>
2023-07-03 18:34:24 -0400

    update views

--- main_app/models.py
+++ main_app/models.py
@@ -6,8 +6,24 @@ class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
 
+    def num_of_ratings(self):
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+    def avg_rating(self):
+        sum = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            sum += rating
+        if len(ratings) > 0:
+            return sum / len(ratings)
+        else:
+            return 0
+
     def __str__(self):
         return self.title
+
+
+
 
 
 
@@ -19,5 +35,5 @@ class Rating(models.Model):
     stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     class Meta:
-        unique_together = (('user', 'movie'))
-        index_together = (('user', 'movie'))
+        unique_together = (('user', 'movie'),)
+        index_together = (('user', 'movie'),)
